76766f14d6be3a872aa943cd4cdc620e3f71de6b
Greg McGee <spongessuck@gmail.com>
2015-02-20 16:53:59 -0500

    Encapsulate module, bump version, add Bower info to Readme

--- src/gm.datepickerMultiSelect.js
+++ src/gm.datepickerMultiSelect.js
@@ -22,74 +22,76 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 */
 
-angular.module('gm.datepickerMultiSelect', ['ui.bootstrap'])
-.config(['$provide', function($provide) {
-	$provide.decorator('daypickerDirective', ['$delegate', function($delegate) {
-		var directive = $delegate[0];
-		
-		/* Override compile */
-		var link = directive.link;
+(function() {
+	angular.module('gm.datepickerMultiSelect', ['ui.bootstrap'])
+	.config(['$provide', function($provide) {
+		$provide.decorator('daypickerDirective', ['$delegate', function($delegate) {
+			var directive = $delegate[0];
 
-		directive.compile = function() {
-			return function(scope, element, attrs, ctrl) {
-				link.apply(this, arguments);
-				
-				var selectedDates = [];
-				
-				/* Called when multiSelect model is updated */
-				scope.$on('update', function(event, newDates) {
-					selectedDates = newDates;
-					update();
-				});
-				
-				/* Get dates pushed into multiSelect array before Datepicker is ready */
-				scope.$emit('requestSelectedDates');
-				
-				/* Fires when date is selected or when month is changed. */
-				scope.$watch(function () {
-					return ctrl.activeDate.getTime();
-				}, update);
-				
-				function update() {
-					angular.forEach(scope.rows, function(row) {
-						angular.forEach(row, function(day) {
-							day.selected = selectedDates.indexOf(day.date.setHours(0, 0, 0, 0)) > -1
-						});
+			/* Override compile */
+			var link = directive.link;
+
+			directive.compile = function() {
+				return function(scope, element, attrs, ctrl) {
+					link.apply(this, arguments);
+
+					var selectedDates = [];
+
+					/* Called when multiSelect model is updated */
+					scope.$on('update', function(event, newDates) {
+						selectedDates = newDates;
+						update();
 					});
+
+					/* Get dates pushed into multiSelect array before Datepicker is ready */
+					scope.$emit('requestSelectedDates');
+
+					/* Fires when date is selected or when month is changed. */
+					scope.$watch(function () {
+						return ctrl.activeDate.getTime();
+					}, update);
+
+					function update() {
+						angular.forEach(scope.rows, function(row) {
+							angular.forEach(row, function(day) {
+								day.selected = selectedDates.indexOf(day.date.setHours(0, 0, 0, 0)) > -1
+							});
+						});
+					}
 				}
 			}
-		}
-		
-		return $delegate;
-	}]);
-}])
-.directive('multiSelect', function() {
-	return {
-		require: ['ngModel'],
-		link: function(scope, elem, attrs, ctrls) {
-			var selectedDates;
-			
-			/* Called when directive is compiled */
-			scope.$on('requestSelectedDates', function() {
-				scope.$broadcast('update', selectedDates);
-			});
-			
-			scope.$watchCollection(attrs.multiSelect, function(newVal) {
-				selectedDates = newVal || [];
-				scope.$broadcast('update', selectedDates);
-			});
 
-			scope.$watch(attrs.ngModel, function(newVal, oldVal) {
-				if(!newVal) return;
-				
-				var dateVal = newVal.getTime();
+			return $delegate;
+		}]);
+	}])
+	.directive('multiSelect', function() {
+		return {
+			require: ['ngModel'],
+			link: function(scope, elem, attrs, ctrls) {
+				var selectedDates;
 
-				if(selectedDates.indexOf(dateVal) < 0) {
-					selectedDates.push(dateVal);
-				} else {
-					selectedDates.splice(selectedDates.indexOf(dateVal), 1);
-				}
-			});
+				/* Called when directive is compiled */
+				scope.$on('requestSelectedDates', function() {
+					scope.$broadcast('update', selectedDates);
+				});
+
+				scope.$watchCollection(attrs.multiSelect, function(newVal) {
+					selectedDates = newVal || [];
+					scope.$broadcast('update', selectedDates);
+				});
+
+				scope.$watch(attrs.ngModel, function(newVal, oldVal) {
+					if(!newVal) return;
+
+					var dateVal = newVal.getTime();
+
+					if(selectedDates.indexOf(dateVal) < 0) {
+						selectedDates.push(dateVal);
+					} else {
+						selectedDates.splice(selectedDates.indexOf(dateVal), 1);
+					}
+				});
+			}
 		}
-	}
-});
+	});
+})();
