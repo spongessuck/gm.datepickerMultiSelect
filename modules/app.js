14c160c6d6fe15934001121a9dfb1aca31c97d27
Breno Vieira <bvieira@americastg.com>
2015-10-15 17:34:29 -0300

    Fixing issue when more than one datepicker is visible

--- modules/app.js
+++ modules/app.js
@@ -7,4 +7,12 @@ angular.module('app', ['gm.datepickerMultiSelect'])
   this.removeFromSelected = function(dt) {
     this.selectedDates.splice(this.selectedDates.indexOf(dt), 1);
   }
+  
+  this.activeDate1;
+  this.selectedDates1 = [new Date().setHours(24, 0, 0, 0)];
+  this.type1 = 'individual';
+  
+  this.removeFromSelected1 = function(dt) {
+    this.selectedDates1.splice(this.selectedDates1.indexOf(dt), 1);
+  }
 });
